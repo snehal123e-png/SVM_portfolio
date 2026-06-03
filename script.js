09193959f3f983f5ae6b55520ca21a6c2ca52918
Snehal Mankar <snehal123e@gmail.com>
2026-06-03 20:25:01 +0530

    Add menu toggle functionality to navigation

--- script.js
+++ script.js
@@ -218,3 +218,10 @@ if (contactForm) {
 console.log(
     "Portfolio Developed by Snehal Mankar 🚀"
 );
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
